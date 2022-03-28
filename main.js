dab501680ec6a507ca7e59957142b4e86ebecdb7
MurphinSmurfin <alex.hiew23@gmail.com>
2022-03-28 15:44:47 +0800

    Something is brewing

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
+const fs = require('fs');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.once('ready', () => {
-	console.log('Up and running!');
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
 });
+
+client.commands = new Collection();
+client.servers = new Map();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
 client.login(token);
