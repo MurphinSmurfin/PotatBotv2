f04639d23b2413b4400bc5ff3669eb96985fb306
MurphinSmurfin <alex.hiew23@gmail.com>
2022-03-29 16:18:12 +0800

    found the missing link

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('pong!'),
+		.setDescription('Ping pong!'),
+	name: 'ping',
+	description: 'Ping pong!',
+	usage: '',
+	category: 'Fun',
 
 	async execute(message) {
 		await message.reply('pong!');
